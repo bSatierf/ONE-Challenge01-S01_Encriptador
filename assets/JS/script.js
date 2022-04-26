29b3b570f2639d2110e8c656fc4f3399f2453071
bSatierf <balbino.fn@gmail.com>
2022-04-26 00:35:26 -0300

    Ajustes css, teste darkmode

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,15 @@
 
+const body = document.querySelector('body');
+const html = document.querySelector('html')
+const toggle = document.querySelector('.toggle');
+const text = document.querySelector('.indicator')
+toggle.onclick = function() {
+        toggle.classList.toggle('dark-mode');
+        body.classList.toggle('dark-mode');
+        html.classList.toggle('dark-mode');
+}
+
+
 
 
 
